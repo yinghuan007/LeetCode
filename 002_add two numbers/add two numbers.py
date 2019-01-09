045bfa79dacdabd0658e17e6d8b1173c53ba43c6
yinghuan007 <46514716+yinghuan007@users.noreply.github.com>
2019-01-09 14:20:39 +0800

    Update add two numbers.py

--- 002_add two numbers/add two numbers.py
+++ 002_add two numbers/add two numbers.py
@@ -1,1 +1,31 @@
+'''
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+Example:
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+'''
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        re = ListNode(0)
+        r = re
+        carry = 0
+        while(l1 or l2):
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            sum = x + y + carry
+            carry = sum//10
+            r.next = ListNode(sum%10)
+            r = r.next
+            if(l1!=None): l1 = l1.next
+            if(l2!=None): l2 = l2.next
+        if(carry!=0):
+            r.next = ListNode(1)
+        return re.next
